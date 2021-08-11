61b0dbcd90d6dbf7dd665971db7ffb2e81264933
Raffes <rafaelaperes215@gmail.com>
2021-08-10 22:44:57 -0300

    Arquivos adicionado ao projeto

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// let password = document.getElementById("passwordRegister")
+// let confirmPassword = document.getElementById("ConPasswordRegister")
+
+function validar() {
+
+    let password = formRegisterUser.passwordRegister.value
+    let confirmPassword = formRegisterUser.ConPasswordRegister.value
+
+    // if(password == '' || password.length <= 12){
+    //     alert('Menor que 12 digitos ou esta vazia');
+    //     formRegisterUser.passwordRegister.focus()
+    //     return false
+    // }
+
+    if(password != confirmPassword){
+        alert('Senhas diferentes');
+        formRegisterUser.ConPasswordRegister.focus()
+        return false
+    }
+
+    if(confirmPassword == '' || confirmPassword.length <= 12){
+            alert('Menor que 12 digitos ou esta vazia');
+            formRegisterUser.ConPasswordRegister.focus()
+            return false
+    }
+}
+
+// password.onchange = validatePassword;
+// confirmPassword.onkeyup = validatePassword;
